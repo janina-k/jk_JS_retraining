e8f849370773ee09d7237d8dc7cf8c17fefdd550
janina-k <kwasny.janina@gmail.com>
2025-03-25 10:00:58 +0100

    homework1

---
+++ externalFile.js
@@ -0,0 +1,26 @@
+// hw1 #6
+
+// let name = "Janina";
+// let lastName = "Kwasny";
+// let studyGroup = "JS retraining"
+// let yearOfBirth = 1986;
+// let isMarried = true;
+
+// let nullVariable = null;
+// let u;
+
+// console.log(typeof(name));
+// console.log(typeof(lastName));
+// console.log(typeof(studyGroup));
+// console.log(typeof(yearOfBirth));
+// console.log(typeof(isMarried));
+// console.log(typeof(nullVariable));
+// console.log(typeof(undefinedVariable));
+
+// console.log(`Year of Birth (Number): ${yearOfBirth}`);
+// console.log(`Marital Status (Boolean): ${isMarried}`);
+// console.log(`First Name (String): ${name}`);
+// console.log(`Last Name (String): ${lastName}`);
+// console.log(`Study Group (String): ${studyGroup}`);
+
+
---
+++ hw1.js
@@ -0,0 +1,69 @@
+// #2
+
+// alert("Kwasny");
+
+// #3
+
+// let horse;
+// let age;
+
+//  horse = "mare";
+//  age = 14;
+
+// console.log(horse);
+// console.log(age);
+
+// horse = age;
+
+// console.log(horse);
+// console.log(age);
+
+// #4
+
+// const horse = {
+//     name: "Ichigo",    
+//     age: 2,
+//     isGelding: true,
+//     isCalm: undefined,
+//     hasSiblings: null
+// };
+
+// console.log(horse);
+
+// #5
+
+// let isAdult = confirm("Are you 18 years old?");
+
+// console.log(isAdult);
+
+// #7
+
+let login = prompt("What's your login?");
+let email = prompt("What's your email address?");
+let password = prompt("What's your password?");
+
+console.log(`Dear: ${login}, your email is: ${email}, your password is ${password}.`);
+
+// #8
+
+// const secondsInMinute = 60;
+// const minutesInHour = 60;
+// const secondsInHour = secondsInMinute * minutesInHour;
+
+// const hoursInDay = 24;
+// const secondsInDay = secondsInHour * hoursInDay;
+
+// const daysInMonth = 30;
+// const secondsInMonth = secondsInDay * daysInMonth;
+
+// console.log(`Seconds in an hour: ${secondsInHour}`);
+// console.log(`Seconds in a day: ${secondsInDay}`);
+// console.log(`Seconds in a month: ${secondsInMonth}`);
+
+
+
+
+
+
+
+
