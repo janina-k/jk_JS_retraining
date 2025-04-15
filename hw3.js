abb73dbef67318339ceb7237ae26a11c4f6aa971
janina-k <kwasny.janina@gmail.com>
2025-04-15 23:32:44 +0200

    update hw3

--- hw3.js
+++ hw3.js
@@ -48,17 +48,56 @@
 
 // #5
 
-function findMin() {
-    let min = arguments[0]
-    for (let i = 1; i < arguments.length; i++){
-       if (min > arguments[i]) {
-        min = arguments[i];
-       }
-    }
-    return min;
-}
+// function findMin() {
+//     let min = arguments[0]
+//     for (let i = 1; i < arguments.length; i++){
+//        if (min > arguments[i]) {
+//         min = arguments[i];
+//        }
+//     }
+//     return min;
+// }
 
-console.log(findMin(12, 14, -4, 0.2));
+// console.log(findMin(12, 14, -4, 0.2));
 
 // #6
 
+// function findUnique(arr) {
+//     for (let i = 0; i < arr.length - 1; i++){
+//         for (let j = i + 1; j < arr.length; j++) {
+//             if (arr[i] == arr[j]){
+//                 return false;
+//             }
+//         }
+//     }
+//     return true;
+// }
+
+// console.log(findUnique([1,2,3,4,5,6]));
+// console.log(findUnique([1,2,3,6,4,5,6]));
+
+// #7
+
+// function lastEl(arr, x) {
+//     if (isNaN(x) ) return arr.slice(-1);
+//     if (x <= 0) return [];
+//     return arr.slice(-x);
+
+// }
+
+// console.log(lastEl([3, 4, 10, -5]));
+// console.log(lastEl([3, 4, 10, -5],2));
+// console.log(lastEl([3, 4, 10, -5],8));
+
+// #8
+
+function capitalFirstLetter(str) {
+    return str
+        .split(' ')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ');
+}
+
+let input = 'i love java script';
+let output = capitalFirstLetter(input);
+console.log(output);
